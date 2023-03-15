0a9aee7be3e790ec9d9e2bd37cf6aca1f5e88997
771073216 <37037387+771073216@users.noreply.github.com>
2023-03-15 11:00:49 +0800

    Update xray.sh

--- files/xray.sh
+++ files/xray.sh
@@ -1,23 +1,4 @@
 if [ "$1" = "configure" ]; then
-  if ! getent group caddy > /dev/null; then
-    groupadd --system caddy
-  fi
-  if ! getent passwd caddy > /dev/null; then
-    useradd --system \
-      --gid caddy \
-      --create-home \
-      --home-dir /var/lib/caddy \
-      --shell /usr/sbin/nologin \
-      --comment "Caddy web server" \
-      caddy
-  fi
-  if getent group www-data > /dev/null; then
-    usermod -aG www-data caddy
-  fi
-  if [ ! -d /var/log/caddy ]; then
-    mkdir -p /var/log/caddy
-    chown -R caddy:caddy /var/log/caddy
-  fi
   if ! sysctl net.ipv4.tcp_congestion_control | grep bbr > /dev/null; then
     sed -i '/net.core.default_qdisc/d' '/etc/sysctl.conf'
     sed -i '/net.ipv4.tcp_congestion_control/d' '/etc/sysctl.conf'
